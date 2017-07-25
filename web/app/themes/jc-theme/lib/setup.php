16ecea95512edb306b127fb755f67c374953f2b7
Jan <mail@janchristlieb.de>
2017-07-25 20:43:48 +0200

    adjusted hero image and deleted unused image settings

--- web/app/themes/jc-theme/lib/setup.php
+++ web/app/themes/jc-theme/lib/setup.php
@@ -37,16 +37,9 @@ function setup()
     // http://codex.wordpress.org/Function_Reference/set_post_thumbnail_size
     // http://codex.wordpress.org/Function_Reference/add_image_size
     add_theme_support('post-thumbnails');
-    add_image_size('1500x400', 1500, 400);
-    add_image_size('1920x1080', 1920, 1080);
-    add_image_size('1950x1300', 1950, 1300);
-    add_image_size('1920x600', 1920, 800, ['center', 'center']);
-    add_image_size('960x540', 960, 540);
-    add_image_size('800x800', 800, 800);
-    add_image_size('600X600', 600, 600);
+    add_image_size('1920x600', 1920, 600, ['center', 'center']);
     add_image_size('360x360', 360, 360);
     add_image_size('800x450', 800, 450);
-    add_image_size('150x150', 150, 150);
     add_image_size('85x85', 85, 85);
     add_image_size('50x50', 50, 50);
 
