595ecd926231b3bc682c70e8f33095dc6118e3dd
Jan <mail@janchristlieb.de>
2017-08-03 13:54:44 +0200

    added contact form for content block template

---
+++ web/app/themes/jc-theme/templates/contact-form-for-content-block.php
@@ -0,0 +1,26 @@
+<section class="contact" id="contact">
+
+    <div class="container">
+        <div class="row content-item">
+            <div class="col-md-12">
+                <?php if ($form_headline = get_field('form_headline')): ?>
+                    <h2 class="anchor" id="<?php echo get_field('form_section_name') ?>"><?php echo($form_headline) ?></h2>
+                <?php endif; ?>
+                <div class="divider"></div>
+                <?php if ($form_about = get_field('form_about')): ?>
+                    <p><?php echo do_shortcode($form_about) ?></p>
+                <?php endif; ?>
+            </div>
+        </div>
+
+        <div class="contact-form">
+            <?php
+            $form = get_field('form_shortcode');
+            ?>
+            <?php if ($form): ?>
+                <?php echo do_shortcode($form) ?>
+            <?php endif; ?>
+        </div>
+
+    </div>
+</section>
