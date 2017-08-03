449b6bcc85af907f2a53587f53e938cf75d4cb47
Jan <mail@janchristlieb.de>
2017-08-03 14:27:01 +0200

    improved code for contact form

--- web/app/themes/jc-theme/templates/contact-form.php
+++ web/app/themes/jc-theme/templates/contact-form.php
@@ -5,7 +5,7 @@
             <div class="col-sm-12">
 
                 <?php if ($form_headline = get_field('form_headline')): ?>
-                    <h2 class="anchor" id="<?php echo get_field('form_section_name') ?>"><?php echo($form_headline) ?></h2>
+                    <h2><?php echo($form_headline) ?></h2>
                 <?php endif; ?>
                 <hr/>
                 <?php if ($form_about = get_field('form_about')): ?>
@@ -17,10 +17,7 @@
         <div class="contact-form">
             <?php
             $form = get_field('form_shortcode');
-            ?>
-            <?php if ($form): ?>
-                <?php echo do_shortcode($form) ?>
-            <?php endif; ?>
+            echo do_shortcode($form) ?>
         </div>
 
     </div>
