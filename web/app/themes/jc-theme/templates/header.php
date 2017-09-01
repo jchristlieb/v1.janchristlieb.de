7739643752f4aa1ab5693b3021a4c79b6271c745
Jan <mail@janchristlieb.de>
2017-09-01 16:35:45 +0200

    styled main navigation

--- web/app/themes/jc-theme/templates/header.php
+++ web/app/themes/jc-theme/templates/header.php
@@ -1,5 +1,5 @@
-<nav id="primary-navigation" class="navbar fixed-top navbar-toggleable-sm navbar-light nav-bg-default">
-    <div class="container ">
+<nav id="primary-navigation" class="navbar fixed-top navbar-toggleable-md navbar-light nav-bg-default">
+    <div class="container">
         <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
                 data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                 aria-label="Toggle navigation">
@@ -8,7 +8,7 @@
         <a class="navbar-brand" href="<?= esc_url(home_url('/#teasers')); ?>"><img class="nav-img"
                                                                                    src="<?= get_stylesheet_directory_uri(); ?>/dist/images/jc-logo-white.png"></a>
 
-        <div class="collapse navbar-collapse navbar-toggleable-sm" id="navbarSupportedContent">
+        <div class="collapse navbar-collapse navbar-toggleable-md" id="navbarSupportedContent">
             <?php
             if (has_nav_menu('primary_navigation')) :
                 wp_nav_menu([
