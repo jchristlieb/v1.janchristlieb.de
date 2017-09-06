7d1c0bbb73ff03577bd7e1d48bad00c63d203376
Jan <mail@janchristlieb.de>
2017-09-06 11:56:44 +0200

    adjustment on content block single

--- web/app/themes/jc-theme/templates/content-block-single.php
+++ web/app/themes/jc-theme/templates/content-block-single.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="row">
             <div class="col-12">
-                <p><?php echo get_field('content_block'); ?></p>
+                <?php echo get_field('content_block'); ?>
             </div>
         </div>
     </div>
