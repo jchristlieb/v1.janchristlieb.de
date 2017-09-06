ec6de7c04d4b33ea1c706bbf6526336cd305ebda
Jan <mail@janchristlieb.de>
2017-09-06 10:02:08 +0200

    cleaned up templates

--- web/app/themes/jc-theme/template-default-content.php
+++ web/app/themes/jc-theme/template-default-content.php
@@ -6,10 +6,6 @@
 
 <?php
 get_template_part('templates/site-intro');
-get_template_part('templates/qa-modal');
-get_template_part('templates/toc');
-get_template_part('templates/content-block-headline-icon');
-
 ?>
 
 
